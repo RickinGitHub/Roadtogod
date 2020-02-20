80c1c29cf48f0818f29ec5859b499f438452c887
rick <836384407@qq.com>
2020-02-20 18:06:19 +0800

    [docs]create basic file.

---
+++ include/guidestar.h
@@ -0,0 +1,8 @@
+/****************************************************
+	> File Name: guidestar.h
+	> Author: rick
+	> Mail: horistudio@163.com 
+	> Created Time: Thu 20 Feb 2020 06:01:53 PM CST
+ ****************************************************/
+
+/* Rick's Plan */ 
