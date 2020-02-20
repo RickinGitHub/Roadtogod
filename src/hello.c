450a6b04ac4ddc8112f3c828e9dea9bcc6663cc8
rick <836384407@qq.com>
2020-02-20 18:06:19 +0800

    [docs]create basic file.

---
+++ src/hello.c
@@ -0,0 +1,16 @@
+/****************************************************
+	> File Name: hello.c
+	> Author: rick
+	> Mail: horistudio@163.com 
+	> Created Time: Thu 20 Feb 2020 05:59:15 PM CST
+ ****************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Road to god.\n");
+
+	return 0;
+}
+
